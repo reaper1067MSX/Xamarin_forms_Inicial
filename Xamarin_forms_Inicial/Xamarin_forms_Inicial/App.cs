fe6f6752aa606661cdf28c113ce933bf7c45a08e
reaper1067MSX <geovanny1067@hotmail.com>
2016-10-11 23:48:31 -0500

    Primer subida del proyecto - tpo SHARED Xamarin.forms

---
+++ Xamarin_forms_Inicial/Xamarin_forms_Inicial/App.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Xamarin_forms_Inicial
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// Pantalla principal
+			MainPage = new ContentPage {
+                //Contenido (PANEL) de la pantalla principal
+				Content = new StackLayout {
+                    //Seteo de controles en el PANEL
+                    VerticalOptions = LayoutOptions.Center,
+                    //Controles heredados del Layout
+                    Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms! - El peluca sapbeee..!"
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
